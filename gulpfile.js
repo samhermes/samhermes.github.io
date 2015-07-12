baeb227ac6e2214bd6c316d9a01ee60be41f1f21
Sam Hermes <samhermes@me.com>
2015-07-12 00:35:44 -0500

    Remove posts

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var s3 = require("gulp-s3");
-var aws = require("./aws.json");
-
-
-gulp.task('default', function() {
-  gulp.src('./_site/**')
-    .pipe(s3(aws));
-});
