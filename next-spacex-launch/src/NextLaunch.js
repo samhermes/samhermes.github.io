458ca6c79d25a61cec8284192669df575decfaf4
Sam Hermes <sam.hermes@wustl.edu>
2018-06-20 16:52:39 -0500

    Add basic loading state, add rocket and launch site

--- next-spacex-launch/src/NextLaunch.js
+++ next-spacex-launch/src/NextLaunch.js
@@ -5,7 +5,8 @@ class NextLaunch extends Component {
   constructor() {
     super();
     this.state = {
-      nextLaunch: '',
+      isLoaded: false,
+      nextLaunch: [],
     }
   };
 
@@ -14,25 +15,43 @@ class NextLaunch extends Component {
     .then(results => {
       return results.json();
     }).then(data => {
-      this.setState({nextLaunch: data})
+      this.setState({
+        isLoaded: true,
+        nextLaunch: data
+      });
     });
   }
 
   render() {
-    return(
-      <div className="next-launch">
-        <h2 className="next-launch-date">
-          <Moment format="MMMM DD, YYYY">{this.state.nextLaunch.launch_date_local}</Moment>
-        </h2>
-        <p className="next-launch-time">
-          <Moment format="H:mm a">{this.state.nextLaunch.launch_date_local}</Moment>
-        </p>
-        <ul className="launch-detail">
-          <li><span className="detail-title">Mission</span> {this.state.nextLaunch.mission_name}</li>
-          <li><span className="detail-title">Flight Number</span> {this.state.nextLaunch.flight_number}</li>
-        </ul>
-      </div>
-    )
+    const { error, isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div class="loading-state"><p>Loading...</p></div>;
+    } else {
+      return(
+        <div className="next-launch">
+          <div className="launch-time">
+            <h2 className="next-launch-date">
+              <Moment format="MMMM DD, YYYY">{this.state.nextLaunch.launch_date_local}</Moment>
+            </h2>
+            <p className="next-launch-time">
+              <Moment format="H:mm a">{this.state.nextLaunch.launch_date_local}</Moment>
+            </p>
+          </div>
+          <div className="launch-details">
+            <ul className="launch-detail">
+              <li><span className="detail-title">Mission</span> {this.state.nextLaunch.mission_name}</li>
+              <li><span className="detail-title">Flight Number</span> {this.state.nextLaunch.flight_number}</li>
+            </ul>
+            <ul className="rocket-detail">
+              <li><span className="detail-title">Rocket</span> {this.state.nextLaunch.rocket.rocket_name}</li>
+            </ul>
+            <ul className="launch-site-detail">
+              <li><span className="detail-title">Launch Site</span> {this.state.nextLaunch.launch_site.site_name_long}</li>
+            </ul>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
